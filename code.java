24ab9d65064ee528c14415880cfce593a8508bc5
PoorvajHV <157215167+PoorvajHV@users.noreply.github.com>
2024-01-22 01:30:10 +0530

    feature signup working

--- code.java
+++ code.java
@@ -1,7 +1,15 @@
 package LocalRepo;
 
+import java.util.Scanner;
+
 public class code {
     public static void main(String[] args){
-        System.out.println("Hello World");
+        Scanner sc = new Scanner (System. in) ;
+        int num = sc.nextInt();
+        if(num % 2 == 0){
+            System.out.println("Even number.");
+        } else {
+            System.out.println("Odd number.");
+        }
     }
 }
